c1a32fb393c23e83ff3991dda0e2f03a27c1dfc9
Larrode Alexis <alexis.larrode@gmail.com>
2021-02-23 15:40:19 +0100

    Create auth.vue

---
+++ pages/auth.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container">
+    <div>
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Authentification admin</span>
+        </div>
+      </nav>
+
+      <form @submit.prevent="submit()">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">E-mail :</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            v-model="email"
+          />
+        </div>
+
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label"
+            >Mot de passe :
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            v-model="password"
+          />
+        </div>
+
+        <button type="submit" class="btn btn-primary">Se connecter</button>
+        
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+
+  methods: {
+    submit() {
+      this.$emit("submit", {
+        email: this.email,
+        password: this.password
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
